ac302b4d5111594509c7be39e1fc91ec34b46584
Kaung Htet Kyaw <kaunghtetkyaw02749@gmail.com>
2020-09-09 18:49:36 +0630

    added profile url card

---
+++ components/Author/UserSocialLinksDashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="white bs-border">
+    <v-container>
+      <v-row dense v-for="url in socialLinks" :key="url">
+        <v-col cols="12" sm="4">
+          <span class="font-weight-bold">{{capitalize(url)}}</span>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <span class="text-body-2 opacity-7">{{user.profile_url[url]?user.profile_url[url]:'-'}}</span>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { capitalize } from "@/utils/utils";
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    capitalize(url) {
+      return capitalize(url);
+    },
+  },
+  computed: {
+    socialLinks() {
+      const keys = Object.keys(this.user.profile_url);
+      return keys;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/dashboard/users/_id/index.vue
+++ pages/dashboard/users/_id/index.vue
@@ -3,17 +3,20 @@
     <h1 v-if="$fetchState.pending">#Loading</h1>
     <div v-else>
       <user-info :user="user"></user-info>
+      <user-links :user="user" class="my-4"></user-links>
     </div>
   </div>
 </template>
 
 <script>
 import UserInfo from "@/components/Author/UserInfoDashboard";
+import UserSocialLinks from "@/components/Author/UserSocialLinksDashboard";
 import { fetchUser } from "@/services/Firebase/userAuth";
 import { mapGetters } from "vuex";
 export default {
   components: {
     "user-info": UserInfo,
+    "user-links": UserSocialLinks,
   },
   async fetch() {
     const user = await fetchUser(this.userID);
